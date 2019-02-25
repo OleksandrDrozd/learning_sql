56798df11c1db0cb0d28480b6c416e21a132c9c1
Oleksandr Drozd <drozdoleksandr@gmail.com>
2019-02-25 16:02:14 +0100

    Created a separate file for list functions

--- db_functions.py
+++ db_functions.py
@@ -1,59 +1,66 @@
 import config as c
-import list_functions as l
+import list_functions as lf
 
 
 # Function adds a single record to contact_details table.
 def add_a_record_to_contact_details(f_name, l_name, c_name):
-    c.my_cursor.execute("INSERT INTO contact_details (first_name, last_name, company_name) "
+    c.mysql_cursor.execute("INSERT INTO contact_details (first_name, last_name, company_name) "
                         "VALUES ('{}', '{}', '{}');"
-                        .format(f_name, l_name, c_name))
-    c.database_connection.commit()
+                           .format(f_name, l_name, c_name))
+    c.mysql_database_connection.commit()
 
 
 # Function adds a list of records to contact_details table.
 def add_multiple_records_to_contact_details(list_of_data):
-    c.my_cursor.executemany("INSERT INTO contact_details (first_name, last_name, company_name) "
-                        "VALUES (%s, %s, %s);", (list_of_data))
-    c.database_connection.commit()
+    c.mysql_cursor.executemany("INSERT INTO contact_details (first_name, last_name, company_name) "
+                        "VALUES (%s, %s, %s);", list_of_data)
+    c.mysql_database_connection.commit()
 
 
 # Function updates a single record in contact_details table using primary key.
 def update_contact_details(f_name, l_name, c_name, p_key):
-    c.my_cursor.execute("UPDATE contact_details "
+    c.mysql_cursor.execute("UPDATE contact_details "
                         "SET "
                             "first_name = '{}', "
                             "last_name = '{}' , " 
                             "company_name = '{}' "
                         "WHERE "
                         "primary_key = {};"
-                        .format(f_name, l_name, c_name, p_key))
-    c.database_connection.commit()
+                           .format(f_name, l_name, c_name, p_key))
+    c.mysql_database_connection.commit()
 
+
+# Functions Truncates table.
+def truncate_table(table_name):
+    c.mysql_cursor.execute("TRUNCATE TABLE {};"
+                           .format(table_name))
+    c.mysql_database_connection.commit()
 
 # Function deletes a single row using a primary key.
 def delete_a_record_from_contact_details(p_key):
-    c.my_cursor.execute("DELETE FROM contact_details "
+    c.mysql_cursor.execute("DELETE FROM contact_details "
                         "WHERE "
                              "primary_key = {};"
-                        .format(p_key))
-    c.database_connection.commit()
+                           .format(p_key))
+    c.mysql_database_connection.commit()
 
 
 # Function deletes a list of rows using a primary key.
 def delete_multiple_records_from_contact_details(list_of_ids):
 
     # Transform original list into a list of tuples. From [1, 2, 3] to [(1,), (2,), (3,)]
-    new_list = l.create_list_of_tuples(list_of_ids)
+    new_list = lf.create_list_of_tuples(list_of_ids)
 
     # Run SQL delete query.
-    c.my_cursor.executemany("DELETE FROM contact_details "
+    c.mysql_cursor.executemany("DELETE FROM contact_details "
                             "WHERE "
-                                "primary_key = %s", (new_list))
-    c.database_connection.commit()
+                                "primary_key = %s", new_list)
+    c.mysql_database_connection.commit()
 
 
-# Function returns all data from a contact_details table.
-def select_all_contacts():
-    c.my_cursor.execute("SELECT * FROM contact_details")
-    my_result = c.my_cursor.fetchall()
+# Function returns all data from a given table.
+def select_all(table_name):
+    c.mysql_cursor.execute("SELECT * FROM {};"
+                           .format(table_name))
+    my_result = c.mysql_cursor.fetchall()
     return my_result
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import db_functions as db
 
+import config as c
+
 
 list_of_contacts = [('Khadijah', 'Hinesley', 'Facebook'),
                     ('Marcene', 'Babiarz', 'Google'),
@@ -7,13 +9,17 @@ list_of_contacts = [('Khadijah', 'Hinesley', 'Facebook'),
                     ('Natisha', 'Shockley', 'Microsoft'),
                     ('Annmarie', 'Fester', 'Ebay')]
 
-db.add_multiple_records_to_contact_details(list_of_contacts)
+# db.add_multiple_records_to_contact_details(list_of_contacts)
 
 
-new_list = list(range(46, 51))
-print(new_list)
+# new_list = list(range(66, 71))
+# print(new_list)
 
-db.delete_multiple_records_from_contact_details(new_list)
+# db.delete_multiple_records_from_contact_details(new_list)
+
+# db.truncate_table(c.mysql_table_contact_details)
 
 
-print(db.select_all_contacts())
+# print(db.select_all(c.mysql_table_contact_details))
+
+print(type(list_of_contacts[0]))
